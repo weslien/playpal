9e3dad61fc902e9f43256fa3d71e124fb8831937
Gustav Weslien <gw@enin.se>
2011-12-03 00:23:28 +0100

    leaf

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,14 @@
 package controllers;
 
+import play.Play;
 import play.mvc.Controller;
+import play.mvc.Router;
+import sun.awt.PlatformFont;
 
 public class Application extends Controller {
 
     public static void index() {
+
         render();
     }
 
---
+++ custom/cmscore/app/controllers/Leaf.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gustav
+ * Date: 2011-12-02
+ * Time: 23:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class Leaf extends Controller{
+
+    public static void view(int id){
+
+        //Find all classes with BeforeNodeLoaded watches
+
+
+        render();
+    }
+}
