c8930e46a6d393eb55b177d6b468eb9fac0b81bc
Jens Alm <jens@ulmus.org>
2011-12-21 23:51:35 +0100

    Fixed new tests... that I broke...

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,6 +5,10 @@ import play.mvc.Controller;
 public class Application extends Controller {
 
     public static void index() {
+        //TODO: Check for if config !exists and redirect to wizard
+
+        //TODO: Display front page
+
         render();
     }
 
