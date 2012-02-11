93b98697e3e58a63f05487974eda13a92119273b
Jens Alm <jens@ulmus.org>
2012-02-11 22:27:46 +0100

    Removed some unused stuff.

--- custom/cmsadmin/src/play/modules/origo/admin/Admins.java
+++ custom/cmsadmin/src/play/modules/origo/admin/Admins.java
@@ -1,10 +1,7 @@
 package play.modules.origo.admin;
 
 import org.apache.commons.lang.StringUtils;
-import play.modules.origo.admin.annotations.Admin;
-import play.modules.origo.core.annotations.Provides;
 
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -18,29 +15,6 @@ public class Admins {
     public static void invalidate() {
         pages.clear();
         aliases.clear();
-    }
-
-    public static void addPage(Method m) {
-        Admin.Page pageAnnotation = m.getAnnotation(Admin.Page.class);
-        if (StringUtils.isBlank(pageAnnotation.name())) {
-            throw new RuntimeException("Admin.Page can not have an empty name attribute");
-        }
-        String pageName = getSafeString(pageAnnotation.name());
-        if (pages.contains(pageName)) {
-            throw new RuntimeException("Admin.Page must have a unique name attribute");
-        }
-
-        Provides providesAnnotation = m.getAnnotation(Provides.class);
-        if (providesAnnotation == null) {
-            throw new RuntimeException("Methods annotated with Admin.Page must also be annotated with a @Provides annotation");
-        }
-
-        pages.add(pageName);
-        if (StringUtils.isBlank(pageAnnotation.alias())) {
-            aliases.put(pageAnnotation.name(), pageName);
-        } else {
-            aliases.put(pageAnnotation.alias(), pageName);
-        }
     }
 
     public static String getSafeString(String name) {
